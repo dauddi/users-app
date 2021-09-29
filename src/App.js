d32dd68f8e8f28319b02d34e2b17753c66aac6b2
Davis Okioma <kuntaytc@gmail.com>
2021-09-29 21:01:55 +0300

    removed the hard coded user data

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Form from './components/Form';
 import UserList from './components/UserList'
 
 function App() {
-	let userData = [
-		{"name": "Davis Okioma", "age": "23"},
-		{"name": "Aimee Matrone", "age": "32"},
-		{"name": "Lynn Chep", "age": "45"},
-		{"name": "Willy Dangote", "age": "57"}
-	];
-	const [userState, setUserState] = useState(userData);
+	const [userState, setUserState] = useState([]);
 
 	return (
 		<div className="app">
-			<Form updateUserState={setUserState} currentState={[useState]}/>
+			<Form updateUserState={setUserState} currentState={[...userState]}/>
 			<UserList  data={userState}/>
 		</div>
 	)
