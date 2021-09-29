f11ded1c87db06a6d1e4a60beb7202a061561686
Davis Okioma <kuntaytc@gmail.com>
2021-09-29 19:14:15 +0300

    Added form logic to collect user input and pass state up

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,26 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import './css/form.css'
 import Button from "./UI/Button";
+import User from "../Classes/UserClass";
 
-const Form = () => {
+function Form(props){
+	let newUser;
+	const [nameInput, setNameInput] = useState('')
+	const [ageInput, setAgeInput] = useState('')
+
+	const formSubmitHandler = (e) => {
+		e.preventDefault();
+
+		if (nameInput && ageInput) newUser = new User(nameInput, ageInput)
+		props.updateUserState([newUser, ...props.currentState])
+	}
 
 	return (
-		<form className="form-control">
+		<form onSubmit={formSubmitHandler} className="form-control">
 			<h1>Enter User Details</h1>
 
 			<div className="form-group">
-				<label for="user_name">User Name:</label>
-				<input type="text" name="user_name" required />
+				<label htmlFor="user_name">User Name:</label>
+				<input onChange={(e) => setNameInput(e.target.value)} type="text" name="user_name" required />
 			</div>
 
 			<div className="form-group">
-				<label for="age">Age (years):</label>
-				<input type="number" name="age" min="1" required />
+				<label htmlFor="age">Age (years):</label>
+				<input onChange={(e) => setAgeInput(e.target.value)} type="number" name="age" min="1" required />
 			</div>
 			
 			<div className="form-group form-group-btn">
-				<Button>Add User</Button>
-				<Button hover__color="lightcoral">Clear Form</Button>
+				<Button type='submit'>Add User</Button>
+				<Button>Clear Form</Button>
 			</div>
 			
 		</form>
